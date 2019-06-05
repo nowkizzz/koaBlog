8ba136578069db17e33245b988592052cf25342f
DESKTOP-5SK4FQ2\ava <270113401@qq.com>
2019-06-05 17:33:15 +0800

    to do jwt

--- routes/user.js
+++ routes/user.js
@@ -44,8 +44,12 @@ let addUsers = async (ctx, next) => {
   ctx.response.body = data;
 }
 
+// 更新token
+let refreshToken = async (ctx, next) => {
 
+}
 
+// 登录 用到jwt
 let login = async (ctx, next) => {
   let requestData = ctx.request.body;
   console.log(requestData, 9999999)
@@ -79,8 +83,41 @@ let login = async (ctx, next) => {
   }
 }
 
+// 测试使用jwt的返回 get方法
+let getList = async (ctx, next) => {
+  console.log(33333, ctx.request)
+  let requestData = ctx.params;
+  let headerToken = ctx.request.header.token
+  console.log(headerToken, 444444444)
+  if (headerToken) {
+    try {
+      let isSuccess = tokenTool.verifyToken(headerToken);
+      ctx.response.body = {
+        code: 0,
+        data: [],
+        message: isSuccess
+      }
+    } catch (e) {
+      console.log(222, e)
+      ctx.response.body = {
+        code: 1,
+        data: [],
+        message: e.toString()
+      }
+    }
+  }
+  // ctx.response.body = {
+  //   code: 1,
+  //   data: [],
+  //   message: '请传输token'
+  // }
+
+}
+
+
 module.exports = {
   'GET /api/getUsers': getUsers,
   'POST /api/addUsers': addUsers,
-  'POST /api/login': login
+  'POST /api/login': login,
+  'GET /api/getList': getList,
 }
